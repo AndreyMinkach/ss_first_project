7de0e57ac3226a81d291ddde957b69c44a531fd0
AndreyMinkach <luckydron066@gmail.com>
2020-10-20 18:49:08 +0300

    Add greeting into master.

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class FirstTask {
+public class Greeting {
     public static void main(String[] args) {
         System.out.println("Minkach Andrii\nSoftServe Crash Course");
     }

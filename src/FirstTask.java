6563819d2f90db4d7c9dd762ddb99e60b9d5b0dc
AndreyMinkach <luckydron066@gmail.com>
2020-10-20 18:38:37 +0300

    Add file with first task.

---
+++ src/FirstTask.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        double abc[] = {20.2, 30.3, 10.1};
+        double xy[] = {24, 31};
+        Arrays.sort(abc);
+        Arrays.sort(xy);
+        if (abc[0] <= xy[0] && abc[1] <= xy[1]){
+            System.out.println("You will pass");
+        }
+        else System.out.println("You shall not pass");
+    }
+}

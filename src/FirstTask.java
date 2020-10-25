46b17a414b34b49dd04419a7f55c620c91640d00
AndreyMinkach <luckydron066@gmail.com>
2020-10-25 23:58:19 +0300

    Fix default constructor bug.

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -8,20 +8,18 @@ import java.util.Arrays;
 
 class TaskOne {
     public TaskOne() {
-        new TaskOne(20.2, 30.3, 10.1,12, 31);
+        this(20.2, 30.3, 10.1, 12, 31);
     }
 
     public TaskOne(double a, double b, double c, double x, double y) {
-        double [] abc = {a, b, c};
-        double [] xy = {x, y};
+        double[] abc = {a, b, c};
+        double[] xy = {x, y};
         Arrays.sort(abc);
         Arrays.sort(xy);
         if (abc[0] <= xy[0] && abc[1] <= xy[1]) {
             System.out.println("You will pass");
         } else System.out.println("You shall not pass");
     }
-
-
 }
 
 public class FirstTask {

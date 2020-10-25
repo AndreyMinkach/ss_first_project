490633b6182fe4a421bf60f21d28c8064821d74c
AndreyMinkach <luckydron066@gmail.com>
2020-10-25 23:35:18 +0300

    Update with oop.

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,13 +1,32 @@
+import com.javacourse.se.lesson14.MyArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class FirstTask {
-    public static void main(String[] args) {
-        double abc[] = {20.2, 30.3, 10.1};
-        double xy[] = {10, 31};
+class TaskOne {
+    public TaskOne() {
+        new TaskOne(20.2, 30.3, 10.1,12, 31);
+    }
+
+    public TaskOne(double a, double b, double c, double x, double y) {
+        double [] abc = {a, b, c};
+        double [] xy = {x, y};
         Arrays.sort(abc);
         Arrays.sort(xy);
         if (abc[0] <= xy[0] && abc[1] <= xy[1]) {
             System.out.println("You will pass");
         } else System.out.println("You shall not pass");
+    }
+
+
+}
+
+public class FirstTask {
+    public static void main(String[] args) throws IOException {
+        TaskOne firstTask = new TaskOne();
+
     }
 }

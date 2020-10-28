3ef9dd2b922288b5352b31cb6fd1541b15436cff
AndreyMinkach <luckydron066@gmail.com>
2020-10-28 20:27:34 +0200

    Ready second homework task collections.

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,27 @@
 import java.util.*;
 
 public class Main {
-    public static Set union(Set set1, Set set2){
-        Set temp_set = new HashSet<>(set1);
-        for (Object setElement: set1) {
-            temp_set.add(setElement);
-        }
-        for (Object setElement: set2) {
-            temp_set.add(setElement);
-        }
-        return temp_set;
-    }
-
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        set1.add(1);
-        set1.add(2);
-        set2.add(3);
-        set2.add(4);
-        System.out.println(union(set1, set2).toString());
+        Student student4 = new Student("andrii4", 2);
+        Student student5 = new Student("zndrii5", 3);
+        Student student1 = new Student("qndrii1", 1);
+        Student student2 = new Student("jndrii2", 1);
+        Student student3 = new Student("nndrii3", 2);
 
-        Set<String> set3 = new HashSet<>();
-        Set<String> set4 = new HashSet<>();
-        set3.add("a");
-        set3.add("b");
-        set4.add("c");
-        set4.add("d");
-        System.out.println(union(set3, set4).toString());
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        Student.printStudents(students, 3);
 
-        Map <String, String> personMap = new HashMap<>();
-        personMap.put("Andrii0", "Dron0");
-        personMap.put("Andrii1", "Dron1");
-        personMap.put("Andrii2", "Dron2");
-        personMap.put("Andrii3", "Dron3");
-        personMap.put("Andrii4", "Dron4");
-        personMap.put("Andrii5", "Dron5");
-        personMap.put("Andrii6", "Dron6");
-        personMap.put("Andrii7", "Dron7");
-        personMap.put("Andrii0", "Dron8");
-        personMap.put("Andrii9", "Dron9");
-        System.out.println(personMap.toString());
-        for (Map.Entry<String, String> person : personMap.entrySet()) {
-            System.out.println("Key: " + person.getKey() + " Value: " + person.getValue());
-        }
-        String nameToDelete = "Andrii0";
-        if (personMap.containsKey(nameToDelete)){
-            personMap.remove(nameToDelete);
-        }else{
-            System.out.println("No such name");
-        }
-        System.out.println(personMap.toString());
+        System.out.println("Sorted by course");
+        students.sort(new StudentCourseComparator());
+        System.out.println(students.toString());
 
+        System.out.println("Sorted by name");
+        students.sort(new StudentNameComparator());
+        System.out.println(students.toString());
     }
 }
